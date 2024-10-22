341f83dbb29cb5133db04c2f8ef594395ca34602
Gonzalo Almazan <galmazan@stu.southtexascollege.edu>
2024-10-21 19:03:36 -0500

    difficulty added

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
 import BaseTitle from '@/components/BaseTitle.vue';
+import DifficultyChip from '@/components/DifficultyChip.vue';
 const api = useAPI()
 const question = ref(null)
 const route = useRoute()
@@ -66,6 +67,7 @@ const shuffle = (array) => {
     </div>
 
 </div>
+<DifficultyChip :difficulty="question.difficulty"></DifficultyChip>
 </div>
 <div v-else class="">
   Loading...
